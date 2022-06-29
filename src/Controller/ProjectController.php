2c63726273378a22e8421358fd2804309a929e33
Chloebert <berthier.chloe@wanadoo.fr>
2022-06-29 22:29:03 +0200

    style

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -31,7 +31,7 @@ class ProjectController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $projectRepository->add($project, true);
 
-            return $this->redirectToRoute('roject_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('project_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('project/new.html.twig', [
